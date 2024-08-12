aad95bc1481c3b6e96282c171acc5eb76e0ccfbb
Patrick <patrick.freilinger@wsu.edu>
2024-08-12 16:50:47 -0700

    Apply CSS to use Rewind-UI properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Button, Input, Text } from '@rewind-ui/core';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <Text variant="d3">MTG Window Shopper</Text>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        Use this application to search for cards from multiple storefronts at once.
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Input size="md" color="blue" placeholder="Search for cards"/>
+      <Button color="green">
+        Search
+      </Button>
     </>
   )
 }
