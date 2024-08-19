c3e100a2073b0c1a976e998af2a19b5ce46ae31a
Patrick <patrick.freilinger@wsu.edu>
2024-08-18 18:31:56 -0700

    Fix links to handle geek fortress

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -4,6 +4,11 @@ import { Button, Table } from '@rewind-ui/core';
 import linkImage from '../assets/small-link.png'
 
 const TableRow = (props) => {
+    const domains = {
+      "Mugu Games": "mugugames",
+      "Geek Fortress": "geekfortressgames"
+    }
+
     return (
       <Table.Tr>
         <Table.Td>{props.name}</Table.Td> {/* Name */}
@@ -13,7 +18,7 @@ const TableRow = (props) => {
         <Table.Td>{props.quantity}</Table.Td> {/* Quantity */}
         <Table.Td>{props.location}</Table.Td> {/* Location */}
         <Table.Td>
-          <Button href={"https://mugugames.com" + props.link} target="_blank" as="a" variant="link" size="xs" icon >
+          <Button href={`https://${domains[props.location]}.com` + props.link} target="_blank" as="a" variant="link" size="xs" icon >
             <img src={linkImage}/>
           </Button>
         </Table.Td> 
